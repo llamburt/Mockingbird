9d8f40395ba17e54a11193bcd00df0138235d438
llamburt <leonidlamburt@gmail.com>
2025-02-26 14:14:12 -0500

    Initial commit

---
+++ mockingbird/mock_sec_master/MockSecurityMaster.py
@@ -0,0 +1,51 @@
+print("begin")
+
+'''
+DB name is logos_db
+
+{
+  "_id": "AAPL",
+  "isin": "US0378331005",
+  "cusip": "037833100",
+  "sedol": "2046251",
+  "bloomberg_ticker": "AAPL US Equity",
+  "asset_class": "Equity",
+  "exchange": "NASDAQ",
+  "currency": "USD",
+  "issuer": "Apple Inc.",
+  "dividend_frequency": "Quarterly",
+}
+
+
+{
+  "security_id": "AAPL",
+  "dividend_type": "Cash",
+  "amount": 0.22,
+  "ex_date": "2024-03-01",
+  "record_date": "2024-03-02",
+  "pay_date": "2024-03-15",
+  "source": "Custodian"
+}
+
+{
+  "security_id": "AAPL",
+  "action_type": "Stock Split",
+  "ratio": "4:1",
+  "ex_date": "2020-08-31",
+  "record_date": "2020-09-01",
+  "effective_date": "2020-09-02",
+  "source": "Bloomberg"
+}
+
+{
+  "security_id": "AAPL",
+  "date": "2024-02-23",
+  "prices": [
+    { "source": "Bloomberg", "price": 182.45, "confidence": 0.95 },
+    { "source": "NASDAQ", "price": 182.40, "confidence": 1.00 },
+    { "source": "Refinitiv", "price": 182.30, "confidence": 0.90 }
+  ]
+}
+
+
+'''
